3369d0a296121b29510116a297ef53fd9e46c13c
Dale Snowdon <dksnowdon@gmail.com>
2019-10-15 22:39:39 +0100

    Add basic interactor base class.

--- src/app/core/definitions/interactor.ts
+++ src/app/core/definitions/interactor.ts
@@ -1,6 +1,21 @@
 import { Input } from './input';
 import { Output } from './output';
+import { PresenterOutput, Presenter } from './presenter';
 
 export interface Interactor {
   execute(request: Input): Promise<Output>;
+}
+
+export interface InteractorPresenter {
+  present(): Promise<Output>;
+}
+
+export abstract class BaseInteractor implements Interactor, InterfacePresentor {
+  protected presenter: Presenter;
+
+  async present(request: Input): Promise<Output> {
+    const result = this.execute(request);
+    const response = this.presenter.present(result);
+    return response;
+  }
 }

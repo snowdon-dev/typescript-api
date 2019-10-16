4fe637706afd7d7655dfff94dff3353359083b5a
Dale Snowdon <dksnowdon@gmail.com>
2019-10-16 15:55:23 +0100

    Remove presenter changes.

--- src/app/core/definitions/interactor.ts
+++ src/app/core/definitions/interactor.ts
@@ -5,17 +5,3 @@ import { PresenterOutput, Presenter } from './presenter';
 export interface Interactor {
   execute(request: Input): Promise<Output>;
 }
-
-export interface InteractorPresenter {
-  present(): Promise<Output>;
-}
-
-export abstract class BaseInteractor implements Interactor, InterfacePresentor {
-  protected presenter: Presenter;
-
-  async present(request: Input): Promise<Output> {
-    const result = this.execute(request);
-    const response = this.presenter.present(result);
-    return response;
-  }
-}

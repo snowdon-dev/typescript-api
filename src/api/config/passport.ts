ca5fbd4fbe3abcf62258974dfb18130f5848a351
Dale Snowdon <dksnowdon@gmail.com>
2019-10-16 17:04:28 +0100

    Fix linting.

--- src/api/config/passport.ts
+++ src/api/config/passport.ts
@@ -7,34 +7,36 @@ import { FindUserInput } from '../../app/find-user/find-user.in';
 
 import bcrypt from 'bcryptjs';
 
-passport.serializeUser(function(user: any, done: (arg0: any, arg1: any) => void) {
+passport.serializeUser((user: {}, done: (arg0: {}, arg1: {}) => void): void => {
   done(null, user);
 });
 
-passport.deserializeUser(function(obj: any, done: (arg0: any, arg1: any) => void) {
-  done(null, obj);
+passport.deserializeUser((user: {}, done: (arg0: {}, arg1: {}) => void): void => {
+  done(null, user);
 });
 passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    const findUser: FindUserInteractor = app.container.resolve<FindUserInteractor>('findUserInteractor');
-    const input: FindUserInput = {
-      email: username,
-    };
-    const response: FindUserOutput = await findUser.execute(input);
-    if (!response) {
-      return done('Incorrect username');
-    }
-    const passCmp = bcrypt.compareSync(password, response.user.password);
+  new LocalStrategy(
+    async (username: string, password: string, done: Function): Promise<void> => {
+      const findUser: FindUserInteractor = app.container.resolve<FindUserInteractor>('findUserInteractor');
+      const input: FindUserInput = {
+        email: username,
+      };
+      const response: FindUserOutput = await findUser.execute(input);
+      if (!response) {
+        return done('Incorrect username');
+      }
+      const passCmp = bcrypt.compareSync(password, response.user.password);
 
-    if (passCmp) {
-      return done(null, {
-        firstname: response.user.firstname,
-        lastname: response.user.lastname,
-        email: response.user.email,
-        username: response.user.username,
-        id: response.user.id,
-      });
-    }
-    return done(null, false, { message: 'Incorrect password.' });
-  }),
+      if (passCmp) {
+        return done(null, {
+          firstname: response.user.firstname,
+          lastname: response.user.lastname,
+          email: response.user.email,
+          username: response.user.username,
+          id: response.user.id,
+        });
+      }
+      return done(null, false, { message: 'Incorrect password.' });
+    },
+  ),
 );
--- src/api/controllers/web.ts
+++ src/api/controllers/web.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-export const getIndex = (req: Request, res: Response) => {
+export const getIndex = (req: Request, res: Response): void => {
   res.set('Content-Type', 'text/html');
   res.send(`
         <h1>Authed route</h1>

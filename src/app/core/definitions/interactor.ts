d6c1e56ef5f179dae30a02d32695d4a430712317
Dale Snowdon <dksnowdon@gmail.com>
2019-10-16 15:55:48 +0100

    Remove presenter changes.

--- src/app/core/definitions/interactor.ts
+++ src/app/core/definitions/interactor.ts
@@ -1,6 +1,5 @@
 import { Input } from './input';
 import { Output } from './output';
-import { PresenterOutput, Presenter } from './presenter';
 
 export interface Interactor {
   execute(request: Input): Promise<Output>;
